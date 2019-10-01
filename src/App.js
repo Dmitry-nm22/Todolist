14d31caba63b52914d1ea082bbe109d2bef37097
Dmitry-nm22 <naumenkodima209@gmail.com>
2019-10-01 20:52:54 +0300

    add props

--- src/App.js
+++ src/App.js
@@ -2,21 +2,32 @@ import React from 'react';
 import './App.css';
 import TodoListHeader from "./TodoListHeader";
 import TodoListTasks from "./TodoListTasks";
-import TodoListFooter from "./TodoListFooter ";
+import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
+
+    tasks = [
+        {title: 'JS', isDone: true, priority: 'low'},
+        {title: 'HTML', isDone: true, priority: 'low'},
+        {title: 'CSS', isDone: true, priority: 'low'},
+        {title: 'React', isDone: false, priority: 'low'}
+    ];
+
+    filterValue='All'
+
     render = () => {
+
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader/>
-                    <TodoListTasks/>
-                    <TodoListFooter/>
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue={this.filterValue}/>
                 </div>
             </div>
         );
     }
-}
 
-export default App;
+}
+    export default App;
 
