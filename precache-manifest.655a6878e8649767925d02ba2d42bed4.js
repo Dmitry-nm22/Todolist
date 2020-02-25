0b4736057c17ad55e24ab1e48eece2e2bdbc6c45
Dmitry-nm22 <naumenkodima209@gmail.com>
2020-02-25 23:16:47 +0300

    Updates

---
+++ precache-manifest.655a6878e8649767925d02ba2d42bed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b89d0f76e645f33f335ffcc9f8a4bb",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "d945baf02ab8c2c6a41b",
+    "url": "/Todolist/static/css/main.a1da7050.chunk.css"
+  },
+  {
+    "revision": "4aa5a3ec8e181d3b9efd",
+    "url": "/Todolist/static/js/2.c107b33b.chunk.js"
+  },
+  {
+    "revision": "d945baf02ab8c2c6a41b",
+    "url": "/Todolist/static/js/main.7a033fd9.chunk.js"
+  },
+  {
+    "revision": "f96ff8426624f7c2968c",
+    "url": "/Todolist/static/js/runtime~main.532f3e5b.js"
+  }
+]);
